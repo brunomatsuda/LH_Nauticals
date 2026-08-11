2b2f69beefe79480659190fef47ca7de83ac975e
matsuda <brunohiyane@gmail.com>
2026-08-11 18:56:00 -0300

    fix: ajustando RAW_PATH

--- config/path.py
+++ config/path.py
@@ -1,5 +1,4 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[1] #/home/bruno/lh_nautical_csv
+BASE_DIR = Path(__file__).resolve().parents[1] #/home/bruno/lh_nautical
 RAW_PATH = BASE_DIR/'data/raw'
-print(BASE_DIR)
