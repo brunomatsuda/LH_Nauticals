83515bae6080ce4afb0342108565511c27624c3b
matsuda <brunohiyane@gmail.com>
2026-08-15 14:00:38 -0300

    feat: Adicionando função que gera o banco de dados no postgresql, juntamente ao schema definido em schema.py

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 # Arquivo para chamar a execução de todos os outros
 #1. schema.py
 from scripts.schema import create_schema
+from ingestion.database import gerar_banco
 
 def main():
     create_schema()
@@ -8,3 +9,4 @@ def main():
 
 if __name__=="__main__":
     main()
+    gerar_banco()
